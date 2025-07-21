c4783ccd59aa00c009fda0c616f9a9ba53371024
GolovatyiN <nikita.golovatyi2003@gmail.com>
2025-07-21 19:37:55 +0300

    Homework 3.2

--- lesson3/lesson3.2.py
+++ lesson3/lesson3.2.py
@@ -1,9 +1,31 @@
-def move_last_to_front(lst):
-    if len(lst) <= 1:
-        return lst
-    return [lst[-1]] + lst[:-1]
+lst = [12, 3, 4, 10]
 
-print(move_last_to_front([12, 3, 4, 10]))
-print(move_last_to_front([1]))
-print(move_last_to_front([]))
-print(move_last_to_front([12, 3, 4, 10, 8]))
+if len(lst) > 1:
+    last = lst.pop()
+    lst.insert(0, last)
+
+print(lst)
+
+lst = [1]
+
+if len(lst) > 1:
+    last = lst.pop()
+    lst.insert(0, last)
+
+print(lst)
+
+lst = []
+
+if len(lst) > 1:
+    last = lst.pop()
+    lst.insert(0, last)
+
+print(lst)
+
+lst = [12, 3, 4, 10, 8]
+
+if len(lst) > 1:
+    last = lst.pop()
+    lst.insert(0, last)
+
+print(lst)
