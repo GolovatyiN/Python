0b2c38860ea59d3a7ef68afa952e80db9fd6de7c
GolovatyiN <nikita.golovatyi2003@gmail.com>
2025-07-21 19:55:46 +0300

    Homework 4.2

--- lesson4/lesson4.2.py
+++ lesson4/lesson4.2.py
@@ -1,16 +1,27 @@
-def even_index_sum_times_last(lst):
-    if not lst:
-        return 0
+lst = [0, 1, 7, 2, 4, 8]
+sum_even_index = 0
+for i in range(0, len(lst), 2):
+    sum_even_index += lst[i]
+result = sum_even_index * lst[-1] if lst else 0
+print(result)
 
-    sum_even_index = 0
+lst = [1, 3, 5]
+sum_even_index = 0
+for i in range(0, len(lst), 2):
+    sum_even_index += lst[i]
+result = sum_even_index * lst[-1] if lst else 0
+print(result)
 
-    for i in range(0, len(lst), 2):
-        sum_even_index += lst[i]
+lst = [6]
+sum_even_index = 0
+for i in range(0, len(lst), 2):
+    sum_even_index += lst[i]
+result = sum_even_index * lst[-1] if lst else 0
+print(result)
 
-    result = sum_even_index * lst[-1]
-    return result
-
-print(even_index_sum_times_last([0, 1, 7, 2, 4, 8]))
-print(even_index_sum_times_last([1, 3, 5]))
-print(even_index_sum_times_last([6]))
-print(even_index_sum_times_last([]))
+lst = []
+sum_even_index = 0
+for i in range(0, len(lst), 2):
+    sum_even_index += lst[i]
+result = sum_even_index * lst[-1] if lst else 0
+print(result)
