7f019f375e969548ee452268899050267e52d2b6
GolovatyiN <nikita.golovatyi2003@gmail.com>
2025-07-21 19:53:45 +0300

    Homework 4.1

--- lesson4/lesson4.1.py
+++ lesson4/lesson4.1.py
@@ -1,19 +1,23 @@
-def move_zeros_to_end(lst):
-    result = []
-    zero_count = 0
+lst = [0, 1, 0, 12, 3]
+non_zeros = [x for x in lst if x != 0]
+zeros = [0] * (len(lst) - len(non_zeros))
+lst[:] = non_zeros + zeros
+print(lst)
 
-    for number in lst:
-        if number == 0:
-            zero_count += 1
-        else:
-            result.append(number)
+lst = [0]
+non_zeros = [x for x in lst if x != 0]
+zeros = [0] * (len(lst) - len(non_zeros))
+lst[:] = non_zeros + zeros
+print(lst)
 
-    for i in range(zero_count):
-        result.append(0)
+lst = [1, 0, 13, 0, 0, 0, 5]
+non_zeros = [x for x in lst if x != 0]
+zeros = [0] * (len(lst) - len(non_zeros))
+lst[:] = non_zeros + zeros
+print(lst)
 
-    return result
-
-print(move_zeros_to_end([0, 1, 0, 12, 3]))
-print(move_zeros_to_end([0]))
-print(move_zeros_to_end([1, 0, 13, 0, 0, 0, 5]))
-print(move_zeros_to_end([9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]))
+lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+non_zeros = [x for x in lst if x != 0]
+zeros = [0] * (len(lst) - len(non_zeros))
+lst[:] = non_zeros + zeros
+print(lst)
