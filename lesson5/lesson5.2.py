049996bc8acb25e3ca0755b046d11c77ca2e29c6
GolovatyiN <nikita.golovatyi2003@gmail.com>
2025-07-27 01:17:31 +0300

    Homework 5.2

---
+++ lesson5/lesson5.2.py
@@ -0,0 +1,19 @@
+while True:
+    number1 = int(input("Enter a number: "))
+    number2 = int(input("Enter a second number: "))
+    operation = input("Enter an action (+, -, *, /): ")
+
+    if operation == "+":
+        print(number1 + number2)
+
+    if operation == "-":
+        print(number1 - number2)
+
+    if operation == "*":
+        print(number1 * number2)
+
+    if operation == "/":
+        if number2 != 0:
+            print(number1 / number2)
+        else:
+            print("Error: you can`t divide by zero")
